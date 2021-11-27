b2adc06bf7eb73dde4a73319a184166f9235b230
yillachen <chn.yilla@gmail.com>
2021-11-27 15:03:59 -0500

    feat: added react-native components - view, button, linking

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { firebase } from './src/firebase/config'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import { LoginScreen, HomeScreen, RegistrationScreen, Dashboard } from './src/screens'
 import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
@@ -45,10 +45,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        { user ? (
+        {user ? (
+          <>
           <Stack.Screen name="Home">
             {props => <HomeScreen {...props} extraData={user} />}
-          </Stack.Screen>
+            </Stack.Screen>
+            {/* <Stack.Screen name="Dashboard" component={Dashboard} {...props} /> */}
+          </>
         ) : (
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,3 +1,57 @@
-import styles from './styles';
-import { firebase } from '../../firebase/config'
-import React, { useEffect, useState } from 'react'
+import styles from "./styles";
+import { firebase } from "../../firebase/config";
+import React, { useEffect, useState } from "react";
+import { View, Image, Linking } from "react-native";
+import { Button } from "react-native-elements";
+
+export default function Dashboard(props) {
+  return (
+    <>
+      <h1>Welcome home, {props.fullName}</h1>
+      <View style={styles.grid}>
+        <Button style={styles.dashIcon} onPress={() => Linking.openURL('http://google.com')}>
+          <h3>Map</h3>
+          <Image
+            style={styles.dashImg}
+            source={require("../../../assets/icons/map.png")}
+          />
+        </Button>
+        <Button style={styles.dashIcon} onPress={() => Linking.openURL('http://google.com')}>
+          <h3>Safety Net</h3>
+          <Image
+            style={styles.dashImg}
+            source={require("../../../assets/icons/friends.png")}
+          />
+        </Button>
+        <Button style={styles.dashIcon} onPress={() => Linking.openURL('http://google.com')}>
+          <h3>Reports</h3>
+          <Image
+            style={styles.dashImg}
+            source={require("../../../assets/icons/reports.png")}
+          />
+        </Button>
+        <Button style={styles.dashIcon} onPress={() => Linking.openURL('http://google.com')}>
+          <h3>Submit a Report</h3>
+          <Image
+            style={styles.dashImg}
+            source={require("../../../assets/icons/addreport.png")}
+          />
+        </Button>
+        <Button style={styles.dashIcon} onPress={() => Linking.openURL('http://google.com')}>
+          <h3>Past Routes</h3>
+          <Image
+            style={styles.dashImg}
+            source={require("../../../assets/icons/history.png")}
+          />
+        </Button>
+        <Button style={styles.dashIcon} onPress={() => Linking.openURL('http://google.com')}>
+          <h3>SOS</h3>
+          <Image
+            style={styles.dashImg}
+            source={require("../../../assets/icons/alert.png")}
+          />
+        </Button>
+      </View>
+    </>
+  );
+}
