3334f0f03653fecc182d5e8d03cad00c80c88c8a
MaxielMrvaljevic <maxielmrvaljevic@gmail.com>
2021-12-01 22:21:02 -0500

    removes map in map.js for functionality due to error rendering app in expo browser view, adds safety nets folder and files

--- App.js
+++ App.js
@@ -9,25 +9,6 @@ import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
-import "react-native-gesture-handler";
-import React, { useEffect, useState } from "react";
-import { firebase } from "./src/firebase/config";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import {
-  LoginScreen,
-  HomeScreen,
-  RegistrationScreen,
-  Dashboard,
-} from "./src/screens";
-import { decode, encode } from "base-64";
-if (!global.btoa) {
-  global.btoa = encode;
-}
-if (!global.atob) {
-  global.atob = decode;
-}
-
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -73,7 +54,6 @@ export default function App() {
               {(props) => <Dashboard {...props} extraData={user} />}
             </Stack.Screen>
             <Stack.Screen name="Map" component={Map} />
-            {/* <Stack.Screen name="Dashboard" component={Dashboard} {...props} /> */}
           </>
         ) : (
           <>
--- src/screens/Dashboard/Map.js
+++ src/screens/Dashboard/Map.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function Map() {
   return (
     <View style={styles.container}>
-      <MapView
+      {/* <MapView
         style={{height: '50%', width: '100%'}} 
         provider={PROVIDER_GOOGLE} //import the provider google
         showsUserLocation={true} //allows to see your current location displayed on the map
-      />
+      /> */}
       <StatusBar style="auto"/>
     </View>
   );
