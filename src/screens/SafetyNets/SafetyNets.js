b89b19b9177188ddd77acd93f01268aecbe6c3bc
maxielmrvaljevic <maxielmrvaljevic@gmail.com>
2021-12-06 18:26:34 -0500

    creates safetyNets array of dummy data

--- src/screens/SafetyNets/SafetyNets.js
+++ src/screens/SafetyNets/SafetyNets.js
@@ -11,3 +11,20 @@ import {
   SmallIcon
 } from "../../../styles";
 
+const safetyNets = [
+  {
+    id: 1,
+    name: "Dance Team",
+    contacts: [{ id: 1, name: "Anita" }, { id: 2, name: "Diane" }, { id: 3, name: "Nick" }],
+  },
+  {
+    id: 2,
+    name: "Study Group",
+    contacts: [{ id: 1, name: "Claudia" }, { id: 2, name: "Josephine" }, { id: 3, name: "Yilla" }],
+  },
+  {
+    id: 3,
+    name: "Roomies",
+    contacts: [{ id: 1, name: "Jamie" }, { id: 2, name: "Julian" }, { id: 3, name: "Jackie" }],
+  },
+];
