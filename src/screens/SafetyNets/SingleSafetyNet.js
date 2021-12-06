78cc63899a24aa03ca0efaf35820f0cab2b057bb
maxielmrvaljevic <maxielmrvaljevic@gmail.com>
2021-12-06 18:40:11 -0500

    imports react and relevant components

---
+++ src/screens/SafetyNets/SingleSafetyNet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Linking } from 'react-native';
+import Header from '../../nav/Header';
+import {
+  Container,
+  DashText,
+  CircularImage,
+  AddButton,
+  Title,
+  SmallIcon,
+  FlexColumnButton,
+  SmallAddButton,
+  Grid
+} from "../../../styles";
