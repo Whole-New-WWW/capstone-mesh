748ad09753304a6d0daf4c7f264f4fef5ef90bef
maxielmrvaljevic <maxielmrvaljevic@gmail.com>
2021-12-06 18:25:20 -0500

    imports react and relevant components to SafetyNets.js

---
+++ src/screens/SafetyNets/SafetyNets.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../nav/Header';
+import {
+  Container,
+  ButtonListContainer,
+  FlexRowButton,
+  DashText,
+  CircularImage,
+  AddButton,
+  Title,
+  SmallIcon
+} from "../../../styles";
+
