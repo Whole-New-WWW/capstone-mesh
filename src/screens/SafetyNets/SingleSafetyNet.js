e63868a9e7936e8c6f54400997f2a11cd2361d5f
maxielmrvaljevic <maxielmrvaljevic@gmail.com>
2021-12-06 18:41:42 -0500

    creates and exports single safety net component

--- src/screens/SafetyNets/SingleSafetyNet.js
+++ src/screens/SafetyNets/SingleSafetyNet.js
@@ -12,3 +12,37 @@ import {
   SmallAddButton,
   Grid
 } from "../../../styles";
+
+export default function SingleSafetyNet(props) {
+  const { safetyNet } = props.route.params;
+  const contacts  = safetyNet.contacts;
+
+  return (
+    <Container>
+      <Header {...props}/>
+      <Title>
+        {safetyNet.name}
+      </Title>
+      <CircularImage source={require('../../../assets/icons/friends.png')}/>
+      <Grid>
+        {contacts.map(contact => {
+          return (
+            <FlexColumnButton 
+              key={contact.id} 
+            >
+              <DashText>
+                {contact.name}
+              </DashText>
+            </FlexColumnButton>
+          )
+        })}
+        <SmallAddButton>
+          <SmallIcon source={require('../../../assets/icons/plus.png')}/> 
+          <DashText>
+            Add Contact
+          </DashText>
+        </SmallAddButton>
+      </Grid>
+    </Container>
+  )
+}
