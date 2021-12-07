221c97281fe05abb34e53891cf9d9ea0f21585f5
maxielmrvaljevic <maxielmrvaljevic@gmail.com>
2021-12-07 00:03:23 -0500

    refactors code to fix bugs in CrimeData component

--- api.js
+++ api.js
@@ -1,15 +1,24 @@
 var axios = require("axios").default;
 
 //testing connection to NYC Crime Data API
-var options = {
-  method: 'GET',
-  url: 'https://data.cityofnewyork.us/resource/qb7u-rbmr.json',
-  params: {"$$app_token" : "r3yihvpeNvCu8FsyP7J2qs00l"} //GET request always pass params
-};
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+export function getManhattanCrimeData() {
+  var options = {
+    method: 'GET',
+    url: 'https://data.cityofnewyork.us/resource/qb7u-rbmr.json',
+    params: { $$app_token: 'r3yihvpeNvCu8FsyP7J2qs00l' }, //GET request always pass params
+  }
+
+  return axios
+    .request(options)
+    .then(function (response) {
+      console.log('HERE IS THE RESPONSE DATA', response.data)
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
+}
+
+
 
---
+++ src/screens/Dashboard/CrimeData.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { getManhattanCrimeData } from '../../../api';
+import { Marker } from 'react-native-maps';
+
+export default function CrimeData() {
+  // initial render will have data and setData = []
+  const [data, setData] = useState([]);
+  // Similar to component did mount
+  useEffect(() => {
+    // await crime data from axios request in api file
+    function fetchCrimeData() {
+      getManhattanCrimeData().then(crimeStats => {
+        // set state for data
+        setData(crimeStats);
+      })
+    }
+    fetchCrimeData();
+  }, []) 
+  // empty array tells useEffect to only run once
+
+  console.log('HERE IS THE CRIME DATA', data);
+
+  return (
+    <>
+    {/* map over crime data and render marker for each point */}
+      {data.map((crime, index) => (
+        <Marker
+          key={index}
+          // Number() used to address error initially being thrown re: invalid type "string" passed into coordinate
+          coordinate={{latitude: Number(crime.latitude), longitude: Number(crime.longitude)}}
+          title={crime.law_cat_cd}
+          description={crime.pd_desc}
+        />
+      ))}
+    </>
+  )
+}
