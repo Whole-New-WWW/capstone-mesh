366b3fdd3b0aa8fda0fd3a41191e4a76a17334e2
yillachen <chn.yilla@gmail.com>
2021-12-06 19:08:59 -0500

    feat: sos button onpress captures coordinates

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Linking } from "react-native";
+import React, { useState, useEffect } from 'react'
+import { Linking } from 'react-native'
 import {
   Title,
   Grid,
@@ -8,50 +8,101 @@ import {
   DashContainer,
   DashText,
   SOS,
-} from "../../../styles";
-import Footer from "../../nav/Footer";
-import { AuthContext } from "../../nav/Auth";
+} from '../../../styles'
+import Footer from '../../nav/Footer'
+import { AuthContext } from '../../nav/Auth'
+
+// Expo iOS Framework
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location' //using expo to get the location of user
 
 export default function Dashboard(props) {
-  let [user] = useState(AuthContext);
-  user = user._currentValue.user;
+  const [location, setLocation] = useState(null)
+  // const [initialRegion, setInitialRegion] = useState(null)
+  let [user] = useState(AuthContext)
+  user = user._currentValue.user
 
   const title = () => {
     if (user) {
-      return `Welcome home, ${user.firstName}`;
+      return `Welcome home, ${user.firstName}`
     } else {
-      return `Welcome home`;
+      return `Welcome home`
     }
-  };
+  }
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await requestForegroundPermissionsAsync() //Asks the user to grant permissions for location
+      if (status != 'granted') {
+        setError('Permission to access location was denied')
+        return
+      }
+      const locate = await getCurrentPositionAsync({});
+      setLocation({
+        latitude: locate.coords.latitude,
+        longitude: locate.coords.longitude,
+      });
+      console.log('in locate', locate.coords)
+    })()
+  }, [])
+
+  // onPress handler to save location
+  const onSOS = async () => {
+    try {
+      console.log('GRABBING LOCATION', location)
+    } catch (e) {
+      alert(e)
+    }
+
+    // //async function used to get request permission of users location while getting their current position
+    // ;async () => {
+    //   let { status } = await requestForegroundPermissionsAsync() //Asks the user to grant permissions for location
+    //   if (status != 'granted') {
+    //     setError('Permission to access location was denied')
+    //     return
+    //   }
+    //   const locate = await getCurrentPositionAsync({})
+
+    //   //gets the current user coordinates
+    //   setLocation({
+    //     latitude: locate.coords.latitude,
+    //     longitude: locate.coords.longitude,
+    //   })
+
+    //   console.log('GRABBING LOCATION', location)
+    // }
+  }
 
   return (
     <>
       <DashContainer>
         <Title>{title()}</Title>
         <Grid>
-          <DashButton onPress={() => props.navigation.navigate("Map")}>
+          <DashButton onPress={() => props.navigation.navigate('Map')}>
             <DashText>Account</DashText>
-            <Icon source={require("../../../assets/icons/account.png")} />
+            <Icon source={require('../../../assets/icons/account.png')} />
           </DashButton>
-          <DashButton onPress={() => props.navigation.navigate("Map")}>
+          <DashButton onPress={() => props.navigation.navigate('Map')}>
             <DashText>Map</DashText>
-            <Icon source={require("../../../assets/icons/map.png")} />
+            <Icon source={require('../../../assets/icons/map.png')} />
           </DashButton>
-          <DashButton onPress={() => props.navigation.navigate("Map")}>
+          <DashButton onPress={() => props.navigation.navigate('Map')}>
             <DashText>Safety Net</DashText>
-            <Icon source={require("../../../assets/icons/friends.png")} />
+            <Icon source={require('../../../assets/icons/friends.png')} />
           </DashButton>
-          <DashButton onPress={() => props.navigation.navigate("Time")}>
+          <DashButton onPress={() => props.navigation.navigate('Time')}>
             <DashText>Submit a Report</DashText>
-            <Icon source={require("../../../assets/icons/addreport.png")} />
+            <Icon source={require('../../../assets/icons/addreport.png')} />
           </DashButton>
-          <SOS onPress={() => Linking.openURL("http://google.com")}>
+          <SOS onPress={() => onSOS()}>
             <DashText>SOS</DashText>
-            <Icon source={require("../../../assets/icons/alert.png")} />
+            <Icon source={require('../../../assets/icons/alert.png')} />
           </SOS>
         </Grid>
       </DashContainer>
       <Footer {...props} />
     </>
-  );
+  )
 }
