97b7a08d5f3fab84cdfd3c4f10d0026085046066
maxielmrvaljevic <maxielmrvaljevic@gmail.com>
2021-12-07 22:28:25 -0500

    WIP creates add safety net modal component

---
+++ src/screens/SafetyNets/AddSafetyNetModal.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Alert, Modal} from "react-native";
+
+export default function AddSafetyNetModal() {
+  return (
+    <>
+      <Modal 
+        animationType={"slide"}
+        transparent={false}
+        visible={visible}
+        onRequestClose={() => {
+          Alert.alert('Your safety net has been saved')
+        }}
+      />
+    </>
+  )
+}
--- src/screens/SafetyNets/SafetyNets.js
+++ src/screens/SafetyNets/SafetyNets.js
@@ -12,9 +12,30 @@ import {
   SmallIcon
 } from "../../../styles";
 
+// 
+// const safetyNets = [
+//   {
+//     id: 1,
+//     name: "Dance Team",
+//     contacts: [{ id: 1, name: "Anita" }, { id: 2, name: "Diane" }, { id: 3, name: "Nick" }],
+//   },
+//   {
+//     id: 2,
+//     name: "Study Group",
+//     contacts: [{ id: 1, name: "Claudia" }, { id: 2, name: "Josephine" }, { id: 3, name: "Yilla" }],
+//   },
+//   {
+//     id: 3,
+//     name: "Roomies",
+//     contacts: [{ id: 1, name: "Jamie" }, { id: 2, name: "Julian" }, { id: 3, name: "Jackie" }],
+//   },
+// ];
+
 export default function SafetyNets(props) {
   const { user } = props.route.params;
   const safetyNets = user.safety_nets;
+  const [modalDisplayed, setModalDisplayed] = useState(false);
+
   return (
     <>
       <Container>
@@ -41,7 +62,9 @@ export default function SafetyNets(props) {
                 </FlexRowButton>
               )
             })}
-            <AddButton>
+            <AddButton
+              onPress={() => setModalDisplayed(!modalDisplayed)} 
+            >
               <SmallIcon source={require('../../../assets/icons/plus.png')}/>
               <DashText>
                 Add New Safety Net
